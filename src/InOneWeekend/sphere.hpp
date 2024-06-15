00b83d2fab0e46b696b1cb1db4c44a8b5d544185
Aleksandr Tarakanov <a.tarakanov@corp.nekki.ru>
2024-06-15 23:10:42 +0400

    hittable_list

---
+++ src/InOneWeekend/sphere.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "hittable.hpp"
+#include "vec3.hpp"
+
+#include <cmath>
+#include <algorithm> 
+
+class sphere : public hittable {
+public: 
+	sphere(const point3& center, double radius) : center(center), radius(std::max(0, radius)) {	}
+	bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+		vec3 oc = center - r.origin();
+		auto a = r.direction().length_squared();
+		auto h = dot(r.direction, oc);
+		auto c = oc.length_squared() - radius * radius;
+		
+
+		auto discriminant = h * h - a * c;
+		if (discriminant < 0)
+			return false;
+
+		auto sqrtd = sqrt(discriminant);
+
+		auto root = (h - sqrtd) / a;
+		if (root <= ray_tmin || ray_tmax <= root) {
+			root = (h + sqrtd) / a;
+			if (root <= ray_tmin || ray_tmax <= root) {
+				return false;
+			}
+		}
+		rec.t = root;
+		rec.p = r.at(rec.t);
+		vec3 outward_normal = (rec.p - center) / radius;
+		rec.set_face_normal(r, outward_normal);
+		
+		return true;
+
+	}
+private: 
+	point3 center;
+	double radius;
+};
